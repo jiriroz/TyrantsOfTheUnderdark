c99a1302b89f9316eb2eaa42457c06f49998caa7
jiriroz <jiri.roznovjak@gmail.com>
2020-02-23 23:53:00 -0500

    Load board

--- board.py
+++ board.py
@@ -1,20 +1,70 @@
+import csv
+from enum import Enum
 
-
+class Color(Enum):
+    WHITE = 1
+    RED = 2
+    BLACK = 3
+    ORANGE = 4
+    BLUE = 5
 
 class Board:
     sites = dict()
 
+    def __str__(self):
+        s = ''
+        for id in self.sites:
+            s += str(self.sites[id]) + '\n'
+        return s
 
 class Site:
+    ID = None
+    name = ''
     adj = []
     capacity = -1
     troops = dict()
     spies = dict()
     isBlack = False
     vpPerTurn = 0
-    influencePerTurn = 0
     vpEndGame = 0
+    influencePerTurn = 0
     isRoute = False
-    name = ''
-    ID = 0
     graphicalMetadata = dict()
+
+    def __str__(self):
+        return ("ID={} name={} adj={} capacity={} troops={} spies={} isBlack={} vpPerTurn={} vpEndGame={} influencePerTurn={} isRoute={}".format(
+            self.ID, self.name, str(self.adj), self.capacity, str(self.troops), str(self.spies), self.isBlack, self.vpPerTurn,
+            self.vpEndGame, self.influencePerTurn, self.isRoute
+        ))
+
+def load_board():
+    rows = []
+    with open('board.csv') as f:
+        rows = [row for row in csv.DictReader(f, delimiter=',')]
+
+    board = Board()
+    for row in rows:
+        site = Site()
+        site.ID = int(row['ID'])
+        site.name = row['Name']
+        site.isRoute = bool(int(row['isRoute']))
+        site.capacity = int(row['capacity'])
+        site.isBlack = bool(int(row['isBlack']))
+        site.vpPerTurn = int(row['vpPerTurn'])
+        site.vpEndGame = int(row['vpEndGame'])
+        site.influencePerTurn = int(row['influencePerTurn'])
+        for color in Color:
+            site.troops[color] = 0
+        site.troops[Color.WHITE] = int(row['numWhiteTroops'])
+        site.adj = [int(x) for x in row['neighborIDs'].split(',')]
+        board.sites[site.ID] = site
+    return board
+        
+board = load_board()
+print (board)
+
+for x in board.sites:
+    s = board.sites[x]
+    print (s.name, s.troops, s.troops[Color.WHITE])
+
+#TODO figure out why the troops aren't copying
