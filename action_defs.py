ce561899c039c6735977874e0cd687fb836a390e
jiriroz <jiri.roznovjak@gmail.com>
2020-03-02 18:49:11 -0500

    More work on actions

--- action_defs.py
+++ action_defs.py
@@ -1,6 +1,39 @@
+import subactions as sa
+from board import Color
+
+# CORE
+
 def soldier(game_state):
-    return
+    return sa.Attack(1).play()
+
+def noble(game_state):
+    yield sa.Influence(1).play()
+
+# DROW
+
+def blackguard(game_state):
+    return sa.Choice(sa.Attack(2), sa.Assasinate())
+
+def bounty_hunter(game_state):
+    return sa.Attack(3)
+
+def master_of_melee(game_state):
+    return sa.Choice(sa.Deploy(4), sa.SupplantAnywhere(Color.WHITE))
+
+def spellspinner(game_state):
+    return sa.Choice(sa.PlaceSpy(), sa.ReturnSpy().then(sa.SupplantAtPreviousSite()))
+
+# DRAGON
+
+def red_dragon(game_state):
+    return sa.Supplant().then(sa.ReturnSpy().then(GainVPForEachSiteUnderTotalControl(1)))
+    
 
 actions_mappings = {
-    'Soldier' : soldier
+    'Soldier' : soldier,
+    'Noble' : noble,
+    'Blackguard' : blackguard,
+    'Bounty Hunter' : bounty_hunter,
+    'Master of Melee-Magthere' : master_of_melee,
+    'Spellspinner', spellspinner
 }
---
+++ subactions.py
@@ -0,0 +1,9 @@
+
+
+class Choice:
+    def __init__(self, choice1, choice2):
+        self.choice1 = choice1
+        self.choice2 = choice2
+
+    def play(self):
+        
